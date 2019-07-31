e1a69601562024c95a40ec862968076b2e360b46
Artur Ptaszek <artur.ptaszek@gmail.com>
2019-07-31 21:39:05 +0200

    Add logging

--- src/sweeper.js
+++ src/sweeper.js
@@ -2,10 +2,11 @@ const fs = require('fs').promises;
 const rimraf = require('rimraf');
 const util = require('util');
 const path = require('path');
-const debug = require('debug');
+const debug = require('debug')('super-sweeper');
 
 const DOWNLOADS_PATH = path.resolve(process.env.HOME, 'Downloads');
-const SCREENSHOTS_PATH = '~/Desktop';
+
+const BEFORE_DATE = new Date().getTime() - 30*24*60*60*1000;
 
 const rimrafP = util.promisify(rimraf);
 
@@ -23,14 +24,16 @@ const getStatsForPaths = async (paths) => {
 };
 
 const getStatsOlderThan = async (dir, beforeTime) => {
+    debug('Removing files in directory %s', dir);
+
     const files = await fs.readdir(dir);
+
+    debug('Reading stats for %O', files);
 
     const filePaths = files
         .map((file) => path.join(dir, file));
 
     const stats = await getStatsForPaths(filePaths);
-
-
 
     return stats
         .filter(({ stats: { mtimeMs } }) => mtimeMs <= beforeTime)
@@ -42,14 +45,25 @@ const removeFile = ({ path, stats }) =>
         ? rimrafP(path)
         : fs.unlink(path);
 
-const removeFiles = (paths) =>
-    Promise.all(paths.map(removeFile));
+const removeFiles = (paths) => {
+    const filePaths = paths.map(({ path }) => path);
+
+    debug('Removing %O', filePaths);
+
+    return Promise.all(paths.map(removeFile));
+};
 
 module.exports = {
     clean: async () => {
-        const monthEarlierDate = new Date().getTime() - 30*24*60*60*1000;
+        console.log(`Removing all files before: ${new Date(BEFORE_DATE).toGMTString()}`);
 
-        const files = (await getStatsOlderThan(DOWNLOADS_PATH, monthEarlierDate));
+        const files = (await getStatsOlderThan(DOWNLOADS_PATH, BEFORE_DATE));
+
+        console.log(`Found ${files.length} files.`);
+        console.log('Removing...');
+
         await removeFiles(files);
+
+        console.log(`Successfully removed ${files.length} files.`);
     }
 };
