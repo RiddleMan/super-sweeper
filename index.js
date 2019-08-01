03457499be984c481aa774a1d101d8d248123af1
Artur Ptaszek <artur.ptaszek@gmail.com>
2019-08-01 20:14:26 +0200

    Add sample command line interaction

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const yargs = require('yargs');
 
-require('./src/sweeper').clean();
+yargs
+    .scriptName('super-sweeper')
+    .usage('Usage: $0 <command> [options]')
+    .option('v', {
+        alias: 'verbose',
+        boolean: true,
+        desc: 'Gimme moar output'
+    })
+    .option('q', {
+        alias: 'quiet',
+        boolean: true,
+        desc: 'No input baby'
+    })
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
