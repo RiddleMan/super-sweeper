75968139f3cc87067f4d01b027b074e4d36e596d
Artur Ptaszek <artur.ptaszek@gmail.com>
2019-08-01 21:48:11 +0200

    Add other options

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ yargs
         boolean: true,
         desc: 'No input baby'
     })
+    .option('r', {
+        alias: 'retention',
+        desc: 'Retention expressed in format: 30d, 4h, 10m',
+        default: '30d'
+    })
+    .option('p', {
+        alias: 'path',
+        desc: 'Path to cleanup'
+    })
+    .option('c', {
+        alias: 'config',
+        string: true,
+        desc: 'Path to config file with paths to cleanup'
+    })
     .help('h')
     .alias('h', 'help')
     .argv;
